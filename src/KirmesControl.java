cdb2d0f4a0ec1401a4b5b3b232aeb4c3a646b174
knb <rma6VBRe!00>
2024-10-07 08:40:58 +0200

    Zweiter Commit

---
+++ src/KirmesControl.java
@@ -0,0 +1,17 @@
+public class KirmesControl {
+
+    public static void main(String[] args) {
+        new KirmesControl();
+    }
+
+    public KirmesControl(){
+        startTheKirmes();
+    }
+
+    private startTheKirmes(){
+
+
+
+    }
+
+}
